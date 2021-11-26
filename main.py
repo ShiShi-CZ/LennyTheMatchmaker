6e986a82ab1b5624045893065b006815b8b0855f
NoiLess <zbynek.max.skola@gmail.com>
2021-11-27 00:19:52 +0100

    Added second feature - assign / remove the role to yourself with a second reaction. Also changed some details like naming of variables etc.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import logging
 from os import environ
 
 logging.basicConfig(level=logging.DEBUG)
-REACTION_EMOJI = "✅"    # :white_check_mark:
+REACTION_OPT_IN = "🔔"    # :bell:
+REACTION_KEEP_ROLE = "🎮"    # :video_game:
 MATCHMAKING_ROLE_ID = int(environ['MATCHMAKING_ROLE_ID'])   # @matchmaking
 GUILD_ID = int(environ['GUILD_ID'])   # Wizard Wars Reborn discord server
 CHANNEL_ID = int(environ['CHANNEL_ID'])     # Channel where the message to react to is located
-REACTION_MESSAGE = int(environ['REACTION_MESSAGE'])    # Message where the reacts should be
+MESSAGE_TO_MONITOR = int(environ['REACTION_MESSAGE'])    # Message where the reacts should be
 BOT_TOKEN = environ['LENNYTOKEN']
 
 
@@ -15,16 +16,19 @@ class Lenny(discord.Client):
     async def on_ready(self):
         print('Ready!')
         self.guild = self.get_guild(GUILD_ID)
-        self.message = await self.guild.get_channel(CHANNEL_ID).fetch_message(REACTION_MESSAGE)
+        self.message = await self.guild.get_channel(CHANNEL_ID).fetch_message(MESSAGE_TO_MONITOR)
         self.matchmaking_role = self.guild.get_role(MATCHMAKING_ROLE_ID)
+        self.opt_in_users = set()
         self.matchmaking_users = set()
 
         # Read reactions on the message at startup
         for reaction in self.message.reactions:
-            if reaction.emoji == REACTION_EMOJI:
-                print('Done')
+            if reaction.emoji == REACTION_KEEP_ROLE:
                 users = await reaction.users().flatten()
-                self.matchmaking_users = {user.id for user in users}
+                self.matchmaking_users = {user.id for user in users}    # set of users who want to have the role always
+            if reaction.emoji == REACTION_OPT_IN:
+                users = await reaction.users().flatten()
+                self.opt_in_users = {user.id for user in users}     # set of users who opt in to having role changed based on their Discord presence
 
 
 intents = discord.Intents.all()
@@ -33,22 +37,33 @@ lenny = Lenny(intents=intents, activity=discord.Game('Magicka: Wizard Wars'))
 
 @lenny.event
 async def on_raw_reaction_add(data):
-    if data.message_id == REACTION_MESSAGE and data.emoji.name == REACTION_EMOJI:
+    if data.message_id == MESSAGE_TO_MONITOR and data.emoji.name == REACTION_KEEP_ROLE:
         lenny.matchmaking_users.add(data.user_id)
+        member = lenny.guild.get_member(data.user_id)
+        await member.add_roles(lenny.matchmaking_role, reason='( ͡° ل͜ ͡°)')
+    elif data.message_id == MESSAGE_TO_MONITOR and data.emoji.name == REACTION_OPT_IN:
+        lenny.opt_in_users.add(data.user_id)
 
 
 @lenny.event
 async def on_raw_reaction_remove(data):
-    if data.message_id == REACTION_MESSAGE and data.emoji == REACTION_EMOJI:
-        lenny.matchmaking_users.discard(data.user_id)      # discard doesn't raise an error if by any chance the user isn't in set
+    member = lenny.guild.get_member(data.user_id)
+    if data.message_id == MESSAGE_TO_MONITOR and data.emoji.name == REACTION_OPT_IN:
+        lenny.opt_in_users.discard(member.id)      # discard doesn't raise an error if by any chance the user isn't in set
+    elif data.message_id == MESSAGE_TO_MONITOR and data.emoji.name == REACTION_KEEP_ROLE and lenny.matchmaking_role in member.roles:
+        lenny.matchmaking_users.discard(member.id)
+        await member.remove_roles(lenny.matchmaking_role, reason='( ͠° ͟ʖ ͡°)')
 
 
 @lenny.event
 async def on_member_update(_, member):
-    if (member.id in lenny.matchmaking_users) and member.activity.name == 'Magicka: Wizard Wars' and lenny.matchmaking_role not in member.roles:
+    # if user is opted in and started playing, add role
+    if member.id in lenny.opt_in_users and member.activity.name == 'Magicka: Wizard Wars' and lenny.matchmaking_role not in member.roles:
         await member.add_roles(lenny.matchmaking_role, reason='( ͡° ل͜ ͡°)')
-
-    if member.id in lenny.matchmaking_users and member.activity.name != 'Magicka: Wizard Wars' and lenny.matchmaking_role in member.roles:
+    # if user is opted in and stopped playing, remove role
+    elif member.id in lenny.opt_in_users and member.id not in lenny.matchmaking_users and member.activity.name != 'Magicka: Wizard Wars' and lenny.matchmaking_role in member.roles:
         await member.remove_roles(lenny.matchmaking_role, reason='( ͠° ͟ʖ ͡°)')
-
+    # if user is matchmaking_user and the role has been removed for any reason, add it back
+    elif member.id in lenny.matchmaking_users and lenny.matchmaking_role not in member.roles:
+        await member.add_roles(lenny.matchmaking_role, reason='( ͡° ل͜ ͡°)')
 lenny.run(BOT_TOKEN)
